eb453d49405ae9616d4b3935966bc53690f92543
Lâm Nguyễn Bá <nguyenbalam2307@gmail.com>
2023-03-27 22:08:20 +0700

    s15

---
+++ src/bai2_ArrayIndexOutOfBoundsException/Bai2.java
@@ -0,0 +1,20 @@
+package bai2_ArrayIndexOutOfBoundsException;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        // write your code here
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử thứ " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
